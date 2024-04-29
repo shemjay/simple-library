6273a4f7b4e7141c4040a1fc0a590b2e75d14216
Shem Jaoko <jaokoshem@gmail.com>
2024-04-29 09:37:38 +0300

    Add local storage usage

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // Define a Book object constructor. DONE!
 // Collect user input for a new book entry (title, author, pages, read). DONE!
-// Create a new Book object with the user input.
-// Initialize an array to store book entries (if not already initialized).
-// Add the new book object to the library array.
+// Create a new Book object with the user input. DONE!
+// Initialize an array to store book entries (if not already initialized). DONE!
+// Add the new book object to the library array. DONE!
 // Convert the library array to JSON.
 // Store the JSON string in local storage.
 // Retrieve the JSON string from local storage.
 // Convert the JSON string back to an array of book objects.
 // Iterate over the library array and display book entries on the website.
-const bookForm = document.querySelector('#bookForm');
+const bookForm = document.querySelector('#book-form');
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const bookPages = document.querySelector('#pages');
 const bookRead = document.querySelector('#read');
 const bookStars = document.querySelectorAll('.stars')
 const submitBtn = document.querySelector('#submit');
-
 const myLibrary = [];
 
+//Book object constructor
 class Book {
     constructor(title, author, numOfPages, status, starRating) {
         this.title = title;
@@ -28,6 +28,7 @@ class Book {
     }
 }
 
+//Collect user input and pass to create instance function
 bookForm.addEventListener('submit', (e) => {
   e.preventDefault()
   let title = bookTitle.value;
@@ -42,12 +43,33 @@ bookForm.addEventListener('submit', (e) => {
     } 
   });
 
-  console.log(title, author, numOfPages, status, starRating);
-  const newBook = new Book(title, author, numOfPages, status, starRating);
-  myLibrary.push(newBook);
-  console.log(myLibrary)
+  createInstance(title, author, numOfPages, status, starRating)
+
+  convertLibrary()
+  
+  bookForm.reset();
   
 })
+
+// create new book instance and store in the array
+function createInstance(title, author, numOfPages, status, starRating) {
+  myLibrary.unshift(new Book(title, author, numOfPages, status, starRating));
+}
+
+// Store and retriev entries in local storage
+function convertLibrary() {
+  const setLibrary = JSON.stringify(myLibrary)
+  localStorage.setItem('myLibrary', setLibrary)
+
+  const getLibrary = localStorage.getItem('myLibrary')
+  const parsedLibrary = JSON.parse(getLibrary)
+
+  console.log(parsedLibrary)
+
+}
+
+
+
 
 
 
