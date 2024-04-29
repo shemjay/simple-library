e2bcdbb0942195386f4fd86536cea446d4bfbd2b
Shem Jaoko <jaokoshem@gmail.com>
2024-04-29 09:47:12 +0300

    Add key value pair for each book in local storage

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 // Create a new Book object with the user input. DONE!
 // Initialize an array to store book entries (if not already initialized). DONE!
 // Add the new book object to the library array. DONE!
-// Convert the library array to JSON.
-// Store the JSON string in local storage.
-// Retrieve the JSON string from local storage.
-// Convert the JSON string back to an array of book objects.
+// Convert the library array to JSON. DONE!
+// Store the JSON string in local storage. DONE!
+// Retrieve the JSON string from local storage. DONE!
+// Convert the JSON string back to an array of book objects. DONE!
 // Iterate over the library array and display book entries on the website.
 const bookForm = document.querySelector('#book-form');
 const bookTitle = document.querySelector('#title');
@@ -15,6 +15,7 @@ const bookPages = document.querySelector('#pages');
 const bookRead = document.querySelector('#read');
 const bookStars = document.querySelectorAll('.stars')
 const submitBtn = document.querySelector('#submit');
+const clearBtn = document.querySelector('#clear');
 const myLibrary = [];
 
 //Book object constructor
@@ -53,21 +54,33 @@ bookForm.addEventListener('submit', (e) => {
 
 // create new book instance and store in the array
 function createInstance(title, author, numOfPages, status, starRating) {
-  myLibrary.unshift(new Book(title, author, numOfPages, status, starRating));
+  myLibrary.push(new Book(title, author, numOfPages, status, starRating));
 }
 
-// Store and retriev entries in local storage
-function convertLibrary() {
-  const setLibrary = JSON.stringify(myLibrary)
-  localStorage.setItem('myLibrary', setLibrary)
-
-  const getLibrary = localStorage.getItem('myLibrary')
-  const parsedLibrary = JSON.parse(getLibrary)
-
-  console.log(parsedLibrary)
-
+// Store entries in local storage
+function storeLibrary() {
+  myLibrary.forEach((book, index) => {
+    const key = `book_${index}`;
+    const value = JSON.stringify(book);
+    localStorage.setItem(key, value);
+  });
 }
 
+//Retrieving entried from local storage and place them in new array
+function retrieveLibrary() {
+  const storedBooks = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key.startsWith('book_')) {
+      const bookJson = localStorage.getItem(key);
+      const book = JSON.parse(bookJson);
+      storedBooks.push(book);
+    }
+  }
+  return storedBooks;
+}
+
+clearBtn.addEventListener('click', localStorage.clear)
 
 
 
